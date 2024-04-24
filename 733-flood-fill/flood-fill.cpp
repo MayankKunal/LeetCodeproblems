0eef38212cdb2b38701a331239827df764f0291e
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-04-24 23:47:54 +0530

    Time: 4 ms (84.74%) | Memory: 16.6 MB (83.62%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -18,12 +18,12 @@ void bfs(vector<vector<int>>&image,int sr,int sc,int color)
   queue<pair<int,int>>q;
   int n=image.size();
   int m=image[0].size();
-  vector<vector<int>>visited(n+1,vector<int>(m+1,0));
+//   vector<vector<int>>visited(n+1,vector<int>(m+1,0));
   q.push({sr,sc});
   int drow[4]={0,-1,0,1};
   int dcol[4]={-1,0,1,0};
   image[sr][sc]=color;
-  visited[sr][sc]=1;
+//   visited[sr][sc]=1;
   while(!q.empty())
   {
     auto it=q.front();
@@ -35,10 +35,10 @@ void bfs(vector<vector<int>>&image,int sr,int sc,int color)
     {
         int nrow=row+drow[i];
         int ncol=col+dcol[i];
-        if(nrow>=0 && nrow<image.size() && ncol>=0 && ncol<image[0].size()&&image[nrow][ncol]==colr && !visited[nrow][ncol])
+        if(nrow>=0 && nrow<image.size() && ncol>=0 && ncol<image[0].size()&&image[nrow][ncol]==colr && image[nrow][ncol]!=color)
         {
             q.push({nrow,ncol});
-            visited[nrow][ncol]=1;
+            // visited[nrow][ncol]=1;
             image[nrow][ncol]=color;
         }
     }
