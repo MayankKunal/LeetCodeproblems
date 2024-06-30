a6fcf4123dc14a19ad2757dfbed6ec26b9719c5c
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-06-30 15:04:35 +0530

    Time: 436 ms (42.02%) | Memory: 175 MB (26.06%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,73 +1,61 @@
 class Solution {
-    int findParent(int x, vector<int>& parent) {
-        if (parent[x] == x) return x;
-        return parent[x] = findParent(parent[x], parent);
+    int findParent(int x,vector<int>&parent)
+    {
+        if(x==parent[x]) return x;
+
+        return findParent(parent[x],parent);
     }
-    
-    bool unionSets(int x, int y, vector<int>& parent, vector<int>& size) {
-        int rootX = findParent(x, parent);
-        int rootY = findParent(y, parent);
-        if (rootX == rootY) return false;
-        
-        if (size[rootX] >= size[rootY]) {
-            parent[rootY] = rootX;
-            size[rootX] += size[rootY];
-        } else {
-            parent[rootX] = rootY;
-            size[rootY] += size[rootX];
+    bool findUnion(int x,int y,vector<int>&graphSize,vector<int>&parent)
+    {
+        x=findParent(x,parent);
+        y=findParent(y,parent);
+        if(x==y) return false;
+
+        if(graphSize[x]>=graphSize[y])
+        {
+            parent[y]=x;
+            graphSize[x]+=graphSize[y];
+        }
+        else
+        {
+             parent[x]=y;
+            graphSize[y]+=graphSize[x];
         }
         return true;
     }
-    
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        vector<int> parentAlice(n + 1), parentBob(n + 1), sizeAlice(n + 1, 1), sizeBob(n + 1, 1);
-        
-        for (int i = 1; i <= n; ++i) {
-            parentAlice[i] = i;
-            parentBob[i] = i;
+        vector<int>parentBob(n+1),parentAlice(n+1),graphSizeBob(n+1,1),graphSizeAlice(n+1,1);
+        for(int i=1;i<=n;i++)
+        {
+            parentBob[i]=i;
+            parentAlice[i]=i;
         }
-        
-        int edgesUsed = 0;
-        
-        // Process type 3 edges first
-        for (const auto& edge : edges) {
-            if (edge[0] == 3) {
-                if (unionSets(edge[1], edge[2], parentAlice, sizeAlice) |
-                    unionSets(edge[1], edge[2], parentBob, sizeBob)) {
-                    ++edgesUsed;
+        int edgeUsed=0;
+          for(auto edge:edges)
+          {
+            if(edge[0]==3)
+            {
+                if(findUnion(edge[1],edge[2],graphSizeAlice,parentAlice)|findUnion(edge[1],edge[2],graphSizeBob,parentBob))
+                {
+                    edgeUsed++;
                 }
             }
-        }
-        
-        // Process type 1 edges for Alice
-        for (const auto& edge : edges) {
-            if (edge[0] == 1) {
-                if (unionSets(edge[1], edge[2], parentAlice, sizeAlice)) {
-                    ++edgesUsed;
-                }
-            }
-        }
-        
-        // Process type 2 edges for Bob
-        for (const auto& edge : edges) {
-            if (edge[0] == 2) {
-                if (unionSets(edge[1], edge[2], parentBob, sizeBob)) {
-                    ++edgesUsed;
-                }
-            }
-        }
-        
-        // Check if both graphs are fully traversable
-        int rootAlice = findParent(1, parentAlice);
-        int rootBob = findParent(1, parentBob);
-        
-        for (int i = 1; i <= n; ++i) {
-            if (findParent(i, parentAlice) != rootAlice || findParent(i, parentBob) != rootBob) {
-                return -1;
-            }
-        }
-        
-        return edges.size() - edgesUsed;
+          }
+
+          for(auto edge:edges)
+          {
+            if(edge[0]==1 && findUnion(edge[1],edge[2],graphSizeAlice,parentAlice)) edgeUsed++;
+            if(edge[0]==2 && findUnion(edge[1],edge[2],graphSizeBob,parentBob)) edgeUsed++;
+          }
+
+          int checkAlice=findParent(1,parentAlice);
+          int checkBob=findParent(2,parentBob);
+          for(int i=1;i<=n;i++)
+          {
+            if((findParent(i,parentAlice)!=checkAlice) || (findParent(i,parentBob)!=checkBob)) return -1;
+          }
+
+          return edges.size()-edgeUsed;
     }
 };
