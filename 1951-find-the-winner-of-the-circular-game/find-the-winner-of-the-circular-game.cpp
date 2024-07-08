4cf9508e8601e2be9dc72a17328d33884ddf1dbf
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-07-08 14:33:53 +0530

    Time: 4 ms (53.80%) | Memory: 7.5 MB (43.98%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,25 +1,22 @@
 class Solution {
+    int help(int index,vector<int>&arr,int k)
+    {
+        if(arr.size()==1)
+        {
+            return arr[0];
+        }
+         index=(index+k)%arr.size();
+        arr.erase(arr.begin()+index);
+       return help(index,arr,k);
+    }
 public:
     int findTheWinner(int n, int k) {
-        
-        queue<int> q;
-    int i=1;
-    while(i<=n){
-    q.push(i);
-    i++;
-    }
-
-    while(q.size()!=1){
-
-        int j=1;
-        while(j<k){
-            int temp=q.front();
-            q.push(temp);
-            q.pop();
-            j++;
+        vector<int>arr(n);
+        k=k-1;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=i+1;
         }
-        q.pop();
+        return help(0,arr,k);
     }
-    return q.front();
-}
 };
