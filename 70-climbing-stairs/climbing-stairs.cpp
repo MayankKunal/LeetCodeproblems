e0d79e0a2349ab733757b979fe40c03e1695de85
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-07-26 20:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (5.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,8 +9,18 @@ public:
     
     return dp[n]=help(n-1,dp)+help(n-2,dp);
   }
+   int topDown(int n)
+   {
+    vector<int>dp(n+1,1);
+       for(int i=2;i<=n;i++)
+       {
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
+   }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return help(n,dp);
+        // return help(n,dp);
+        return topDown(n);
     }
 };
