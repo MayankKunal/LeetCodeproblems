538bac141276f8f3d86bb98280c421f905607ba9
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-09-13 08:49:56 +0530

    Time: 72 ms (29.98%) | Memory: 44.9 MB (20.02%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n=arr.size();
-        vector<int>ans;
-        for(auto query:queries)
-        {
-            int l=max(0,query[0]);
-            int r=min(n,query[1]);
-            int temp=0;
-            for(int i=l;i<=r;i++)
-            {
-                temp^=arr[i];
-            }
-            ans.push_back(temp);
-        }
-        return ans;
+       vector<int>xorPrefix(n);
+     
+       int ans=0;
+       
+       for(int i=0;i<n;i++)
+       {
+          ans^=arr[i];
+          xorPrefix[i]=ans;
+          
+       }
+       vector<int>res;
+       for(auto query:queries)
+       {
+         int l=max(0,query[0]);
+         int r=min(n,query[1]);
+            if(l==0) res.push_back(xorPrefix[r]);
+           else 
+           res.push_back(xorPrefix[r]^xorPrefix[l-1]);  
+       }
+       return res;
     }
 };
