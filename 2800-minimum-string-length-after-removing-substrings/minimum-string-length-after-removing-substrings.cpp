bc628d9cc862c6bb79f1e163ce53d07112a10330
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2024-10-07 10:13:28 +0530

    Time: 12 ms (15.77%) | Memory: 11.5 MB (72.37%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minLength(string s) {
+        
         int n=s.size();
+        stack<char>st;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<s.size();j++)
+    if(!st.empty() && st.top()=='A' && s[i]=='B' ||  !st.empty() && st.top()=='C' && s[i]=='D')
             {
-                if(s[j]=='A' && s[j+1]=='B' || s[j]=='C' && s[j+1]=='D')
-                {
-                   string leftHalf=s.substr(0,j);
-                   string rightHalf=s.substr(j+2,s.size()-j+2);
-                   s=leftHalf+rightHalf;
-                   cout<<s<<" ";
-                }
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
             }
         }
-
-        return s.size();
+        return st.size();
     }
 };
