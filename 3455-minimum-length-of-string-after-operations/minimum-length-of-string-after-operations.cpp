e828ea34e8b661b7b28cbf1ddacdefec3ee0ae7d
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-01-13 11:28:18 +0530

    Time: 59 ms (38.71%) | Memory: 32.5 MB (45.56%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        
+        unordered_map<int,int>mp;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+        int ans=0;
+        for(auto x:mp)
+        {
+             if(x.second<3) ans+=x.second;
+             else
+             {
+                ans+=(x.second%2==0)?2:1;
+             }
+        }
+        return ans;
+    }
+};
