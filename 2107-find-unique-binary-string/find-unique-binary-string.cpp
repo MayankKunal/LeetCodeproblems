941397cd3c4aa18d086637419088415fb9add326
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-02-20 21:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (84.00%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,34 +1,13 @@
 class Solution {
-void help(int o,int z,int n,string temp,unordered_set<string>&st)
-{
-    
-        if(temp.size()==n)
-        {
-        st.insert(temp);
-        return;
-         }
-    if(o<n)
-    {
-        help(o+1,z,n,temp+'1',st);
-    }
-    if(z<n)
-    help(o,z+1,n,temp+'0',st);
-}
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        
-        int n=nums.size();
-        unordered_set<string>st;
-        help(0,0,n,"",st);
-        for(auto num:nums)
-        {
-            st.erase(num);
-        }
-        
-        
-        if(st.size())
-        return *st.begin();
-          
-          return "-1";
+
+     int n=nums.size();
+     string ans="";
+     for(int i=0;i<n;i++)
+     {
+        ans+=(nums[i][i]=='1')?'0':'1';
+     }   
+     return ans;
     }
 };
