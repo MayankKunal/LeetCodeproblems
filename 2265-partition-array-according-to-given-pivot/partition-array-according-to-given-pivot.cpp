c3c5435c8461b7d83b6070f92971544870b703df
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-03-03 21:54:24 +0530

    Time: 5 ms (69.33%) | Memory: 139.4 MB (15.64%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> ans(nums.size());
-        int lessI = 0;
-        int greaterI = nums.size() - 1;
-        for (int i = 0, j = nums.size() - 1; i < nums.size(); i++, j--) {
-            if (nums[i] < pivot) {
-                ans[lessI] = nums[i];
-                lessI++;
-            }
-            if (nums[j] > pivot) {
-                ans[greaterI] = nums[j];
-                greaterI--;
-            }
-        }
-        while (lessI <= greaterI) {
-            ans[lessI] = pivot;
-            lessI++;
-        }
+        int n=nums.size();
+         
+         vector<int>less,greater;
+         int p=0;
+         for(int i=0;i<n;i++)
+         {
+            if(nums[i]<pivot) less.push_back(nums[i]);
+            else if(nums[i]>pivot) greater.push_back(nums[i]);
+            else p++;
+         }
+         vector<int>ans;
+         for(int i=0;i<less.size();i++)
+         {
+            ans.push_back(less[i]);
+         }
+         while(p--) ans.push_back(pivot);
+        for(int i=0;i<greater.size();i++) ans.push_back(greater[i]);
         return ans;
     }
 };
