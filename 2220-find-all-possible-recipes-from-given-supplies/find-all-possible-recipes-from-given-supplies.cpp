8eb3b1e5d2fcd26b99304ce9453946e62af92d91
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-03-21 23:27:12 +0530

    Time: 25 ms (97.82%) | Memory: 101.3 MB (88.72%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -2,56 +2,55 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         
+        int n=recipes.size();
         unordered_set<string>st;
-          vector<string>ans;
-        for(string supply: supplies)
+        for(auto supply:supplies)
         {
             st.insert(supply);
         }
-       int m=recipes.size();
-        int n=ingredients.size();
-        for(int k=0;k<m;k++)
-        {
-           
+unordered_map<string,vector<int>>adj;
+vector<int>inDegree(n,0);
         for(int i=0;i<n;i++)
         {
-             if(st.find(recipes[i])!=st.end()) continue;
-            bool flag=true;
-            for(int j=0;j<ingredients[i].size();j++)
+            for(auto ing:ingredients[i])
             {
-                 if(st.find(ingredients[i][j])==st.end())
-                 {
-                       flag=false;
-                       break;
-                 }
-            }
-            if(flag)
-            {
-               
-                 st.insert(recipes[i]);
+                if(!st.count(ing))
+                {
+                    adj[ing].push_back(i);
+                    inDegree[i]++;
+                }
             }
         }
-        }
-
-         for(int i=0;i<n;i++)
+        queue<int>q;
+        for(int i=0;i<n;i++)
         {
-            bool flag=true;
-            for(int j=0;j<ingredients[i].size();j++)
+            if(inDegree[i]==0)
             {
-                 if(st.find(ingredients[i][j])==st.end())
-                 {
-                       flag=false;
-                       break;
-                 }
-            }
-            if(flag)
-            {
-               
-                 ans.push_back(recipes[i]);
+                q.push(i);
             }
         }
+      vector<string>ans;
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
 
+            ans.push_back(recipes[it]);
+            for(auto idx:adj[recipes[it]])
+            {
+                inDegree[idx]--;
+                if(inDegree[idx]==0)
+                {
+                    q.push(idx);
+                }
+        
+            }
+        }
         return ans;
+
+
+
+
 
     }
 };
