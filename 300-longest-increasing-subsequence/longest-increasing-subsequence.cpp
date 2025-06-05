48a0bd2c3f0c4f5d1979798f970dd8d670c8055d
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-06-05 22:55:25 +0530

    Time: 616 ms (5.67%) | Memory: 296.6 MB (11.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    int help(int i,int n,vector<int>&nums,int prev,vector<vector<int>>&dp)
+
+    int help(int i,int n,int prev,vector<int>&nums,vector<vector<int>>&dp)
     {
         if(i==n)
         {
@@ -7,51 +8,24 @@ class Solution {
         }
         if(dp[i][prev+1]!=-1) return dp[i][prev+1];
         int take=0;
-        if(prev==-1 || nums[prev]<nums[i])
+        if(prev==-1 || nums[i]>nums[prev])
         {
-            take=1+help(i+1,n,nums,i,dp);
+            take=1+help(i+1,n,i,nums,dp);
         }
-        int 
-        notTake=help(i+1,n,nums,prev,dp);
+
+        int notTake=help(i+1,n,prev,nums,dp);
+
 
         return dp[i][prev+1]=max(take,notTake);
+
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
- 
-        //  vector<vector<int>>dp1(n+1,vector<int>(n+2,-1));
-        // return help(0,n,nums,-1,dp);
-        // int res=1;
-        // vector<int>ans(n,1);
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<i;j++)
-        //     {
-        //         if(nums[j]<nums[i])
-        //         {
-        //         ans[i]=max(ans[i],ans[j]+1);
-        //         res=max(res,ans[i]);
-        //         }
-        //     }
-        // }
-        // return res;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i-1;j>=-1;j--)
-            {
-                int take=0;
-        if(j==-1 || nums[j]<nums[i])
-        {
-            take=1+dp[i+1][i+1];
-        }
-              int notTake=dp[i+1][j+1];
-             dp[i][j+1]=max(take,notTake);
-            }
-        }
-         return dp[0][0];
-    }
 
+        vector<vector<int>>dp(n+1,vector<int>(n+2,-1));
+
+        return help(0,n,-1,nums,dp);
+    }
 };
