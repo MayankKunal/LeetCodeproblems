05fdd613041cffb094a0cf219ba9dd358453d8a8
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-07-17 16:29:18 +0530

    Time: 11 ms (18.14%) | Memory: 16 MB (14.64%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,7 +1,7 @@
 class Solution {
-    bool isPossible(int row,int col,int n,vector<string>&board)
+    bool isSafe(int row,int col,int n,vector<string>&board)
     {
-        int r=row;
+      int r=row;
         int c=col;
         while(c>=0)
         {
@@ -26,29 +26,34 @@ class Solution {
            if(board[r--][c++]=='Q') return false;
         }
         return true;
+
     }
-    void solve(int row,int n,vector<string>board,vector<vector<string>>&ans)
+    void help(int row,int n,vector<string>board,vector<vector<string>>&ans)
     {
-        if(row==n)
-        {
+         if(row==n)
+         {
             ans.push_back(board);
             return;
-        }
-        for(int col=0;col<n;col++)
-        {
-            if(isPossible(row,col,n,board))
+         }
+
+         for(int col=0;col<n;col++)
+         {
+            if(isSafe(row,col,n,board))
             {
                 board[row][col]='Q';
-                solve(row+1,n,board,ans);
+                help(row+1,n,board,ans);
                 board[row][col]='.';
             }
-        }
+         }
     }
+    
 public:
     vector<vector<string>> solveNQueens(int n) {
-     vector<string>board(n,string(n,'.'));
-     vector<vector<string>>ans;
-     solve(0,n,board,ans);
-           return ans;
+        
+        vector<string>board(n,string(n,'.'));
+        vector<vector<string>>ans;
+               help(0,n,board,ans);
+               return ans;
+        
     }
 };
