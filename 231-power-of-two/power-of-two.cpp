4fbdb3560ade2ee9d46ae57d75b16bac6b651cae
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-08-09 11:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        if(n<0) return false;
-        n=abs(n);
+        
+        if(n<=0) return false;
+       
         return (!(n&(n-1)));
     }
 };
