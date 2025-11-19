e01fb67a1eb20bf32ae3620afecaadc01a642a4f
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-11-19 23:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (20.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
-    int help(int n)
-    {
-        if(n==0 || n==1) return n;
-        return help(n-1)+help(n-2);
-    }
+     
+    vector<int>dp;
 public:
     int fib(int n) {
         
+      dp.resize(n+1,-1);
         return help(n);
+    }
+    int help(int n)
+    {
+        if(n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=help(n-1)+help(n-2);
     }
 };
