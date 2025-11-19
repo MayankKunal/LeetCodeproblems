f98a8acd75c9278d0179e9c88c9245d0afda5cd6
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-11-19 23:20:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (45.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,19 @@
 class Solution {
-    int topDown(int i,int n,vector<int>&nums,vector<int>&dp)
+    vector<int>dp;
+    int help(int i,int n,vector<int>&nums)
     {
         if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take=nums[i]+topDown(i+2,n,nums,dp);
-        int notTake=topDown(i+1,n,nums,dp);
-        return dp[i]=max(take,notTake);
-    }
-    int bottomUp(vector<int>&nums)
-    {
-        int n=nums.size();
-        vector<int>dp(n+1,0);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++)
-        {
-             int take=nums[i];
-             if(i>1)
-             take+=dp[i-2];
-        int notTake=dp[i-1];
-        dp[i]=max(take,notTake);
-        }
-        return dp[n-1];
+         if(dp[i]!=-1) return dp[i];
+         int take=nums[i]+help(i+2,n,nums);
+         int notTake=help(i+1,n,nums);
+
+         return dp[i]=max(take,notTake);
     }
 public:
     int rob(vector<int>& nums) {
-
-     int n=nums.size();
-    //  vector<int>dp(n,-1);
-    //  return topDown(0,n,nums,dp);   
-    return bottomUp(nums);
+        
+        int n=nums.size();
+        dp.resize(n+1,-1);
+        return help(0,n,nums);
     }
 };
