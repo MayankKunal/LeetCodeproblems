ccd1c5358567eefe54741be54bd364d339743582
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2025-12-16 01:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (75.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -2,25 +2,17 @@ class Solution {
     bool help(int src,vector<vector<int>>&graph,vector<int>&col,int curr)
     {
         col[src]=curr;
-        queue<pair<int,int>>q;
-        q.push({src,curr});
-        while(!q.empty())
-        {
-           auto it=q.front();
-           int node=it.first;
-           int currCol=it.second;
-           q.pop();
-           for(auto adjNode:graph[node])
-           {
-               if(col[adjNode]==-1)
-               {
-                    col[adjNode]=!currCol;
-                    q.push({adjNode,!currCol});
-               }
-               else if(currCol==col[adjNode]) return false;
-           }
-        }
-        return true;
+       
+       for(auto adjNode:graph[src])
+       {
+          if(col[adjNode]==-1)
+          {
+            col[adjNode]=!curr;
+             if(!help(adjNode,graph,col,!curr)) return false;
+          }
+          else if(col[adjNode]==curr) return false;
+       }
+       return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
