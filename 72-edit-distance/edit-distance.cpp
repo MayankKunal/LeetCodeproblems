2d0c37849e378d1feda3db05aa1c8e4d0d0fd61f
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2026-01-10 11:54:13 +0530

    Time: 13 ms (19.91%) | Memory: 13.4 MB (30.73%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,30 +1,30 @@
 class Solution {
-    long long  help(int i,int j,int n,int m,string &word1,string &word2,vector<vector<long long>>&dp)
+    int help(int i,int j,int n,int m,string &word1,string &word2,vector<vector<int>>&dp)
     {
-        if(i==n || j==m) return max((n-i),(m-j));
-          
-          if(dp[i][j]!=-1) return dp[i][j];
-
-          long long take=INT_MAX;
-          if(word1[i]==word2[j])
-          {
-            take=help(i+1,j+1,n,m,word1,word2,dp);
-          }
-          long long notTake=1+min(help(i+1,j,n,m,word1,word2,dp),min(help(i,j+1,n,m,word1,word2,dp),help(i+1,j+1,n,m,word1,word2,dp)));
-
-          return dp[i][j]=min(take,notTake);
+        if(i==n)
+        {
+            return m-(j);
+        }
+        if(j==m)
+        {
+            return n-(i);
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take=INT_MAX;
+        if(word1[i]==word2[j])
+        {
+          take=help(i+1,j+1,n,m,word1,word2,dp);
+        }
+        int notTake=1+min(help(i+1,j,n,m,word1,word2,dp),min(help(i,j+1,n,m,word1,word2,dp),help(i+1,j+1,n,m,word1,word2,dp)));
+         
+         return dp[i][j]=min(take,notTake);
     }
 public:
     int minDistance(string word1, string word2) {
-
-     int n=word1.size();
-     int m=word2.size();
-      
-      vector<vector<long long>>dp(n+1,vector<long long>(m+1,-1));
-
-
-      return help(0,0,n,m,word1,word2,dp);
-
-
+        
+        int n=word1.size();
+        int m=word2.size();
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return help(0,0,n,m,word1,word2,dp);
     }
 };
