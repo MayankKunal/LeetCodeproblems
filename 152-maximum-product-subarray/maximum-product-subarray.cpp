4db2a7ef4b449f6b5c264d098e1324796ab81de6
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2026-04-16 15:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.80%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,29 +1,22 @@
 class Solution {
-    int help(int i,int n,vector<int>&nums,vector<int>&dp)
-    {
-        if(i==n) return 1;
-        if(dp[i]!=-1) return dp[i];
-        
-        int take=nums[i]*help(i+1,n,nums,dp);
-        int notTake=help(i+1,n,nums,dp);
-
-        return dp[i]=max(take,notTake);
-
-    }
 public:
     int maxProduct(vector<int>& nums) {
-      int n=nums.size();
-      int maxProduct=0;
-      for(int i=0;i<n;i++)
-      {
-        int product=1;
-        for(int j=i;j<n;j++)
+        
+        int n=nums.size();
+        int prefix=1;
+        int suffix=1;
+        int ans=nums[0];
+        for(int i=0;i<n;i++)
         {
-               product*=nums[j];
-               maxProduct=max(maxProduct,product);
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+           
+           prefix*=nums[i];
+           suffix*=nums[n-i-1];
+
+           ans=max(ans,max(prefix,suffix));
         }
-      }
-if(n==1) return nums[0];
-      return maxProduct;
+
+        return ans;
     }
 };
